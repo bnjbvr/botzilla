ec4e8ec01f1961f4c454a8267dca7f4bbdc6ff3a
Benjamin Bouvier <public@benj.me>
2020-01-05 22:00:12 +0100

    Fixes #5: Have expand-bug write the bug's base information;

--- src/modules/expand-bug.js
+++ src/modules/expand-bug.js
@@ -1,11 +1,45 @@
+let { request } = require("../utils");
+let cheerio = require("cheerio");
+
 const BUG_NUMBER_REGEXP = /bug (\d+)/g;
 
 async function expandBugNumber(client, roomId, msg) {
-  // Expands "bug 1507820" into "https://bugzilla.mozilla.org/show_bug.cgi?id=1507820".
+  // Expands "bug 1507820" into the bug's title.
   var matches = null;
   while ((matches = BUG_NUMBER_REGEXP.exec(msg)) !== null) {
     let bugNumber = matches[1];
-    let msg = `https://bugzil.la/${bugNumber}`;
+    let url = `https://bugzilla.mozilla.org/show_bug.cgi?id=${bugNumber}`;
+
+    let response = await request(url);
+
+    if (!response || response.statusCode !== 200) {
+      continue;
+    }
+
+    let shortUrl = `https://bugzil.la/${bugNumber}`;
+
+    const $ = cheerio.load(response.body);
+
+    const title = $("head title").text();
+    if (!title.length) {
+      // Probably a private bug! Just send the basic information.
+      client.sendText(roomId, shortUrl);
+      continue;
+    }
+
+    let statusAndAssignee = $('head meta[property="og:description"]').attr(
+      "content"
+    );
+
+    // It contains a string of the form "STATUS (assignee) in JavaScript: Core".
+    // Remove what's after the parenthesis (hacky :)).
+    statusAndAssignee = statusAndAssignee.substring(
+      0,
+      statusAndAssignee.indexOf(")") + 1
+    );
+
+    let msg = `${shortUrl} — ${statusAndAssignee} — ${title}`;
+
     client.sendText(roomId, msg);
   }
 }
