b7dcf74efeb3ada9c91f30c08909469edfbf0ce0
Benjamin Bouvier <public@benj.me>
2020-02-12 11:04:15 +0100

    expand-bug: also catch full bugzilla URLs; (fixes #13)

--- src/modules/expand-bug.js
+++ src/modules/expand-bug.js
@@ -4,60 +4,70 @@
 let { request } = require("../utils");
 
 const BUG_NUMBER_REGEXP = /[Bb]ug (\d+)/g;
+const BUG_URL_REGEXP = /https:\/\/bugzilla\.mozilla\.org\/show_bug\.cgi\?id=(\d+)/g;
 
 const COOLDOWN_TIME = 120000; // milliseconds
 
 // Map roomId to bug-number to timer.
 const cooldown = new Map();
 
+async function handleBug(client, roomId, bugNumber, includeUrl) {
+  let roomCooldown = cooldown.get(roomId);
+  if (typeof roomCooldown !== "undefined") {
+    if (typeof roomCooldown.get(bugNumber) !== "undefined") {
+      // Skip to avoid spamming the room.
+      return;
+    }
+  } else {
+    roomCooldown = new Map();
+    cooldown.set(roomId, roomCooldown);
+  }
+
+  let url = `https://bugzilla.mozilla.org/rest/bug/${bugNumber}?include_fields=summary,assigned_to,status,resolution`;
+
+  let response = await request(url);
+  if (!response) {
+    return;
+  }
+
+  let shortUrl = `https://bugzil.la/${bugNumber}`;
+  if (response.statusCode === 401) {
+    // Probably a private bug! Just send the basic information.
+    if (includeUrl) {
+      client.sendText(roomId, shortUrl);
+    }
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    return;
+  }
+
+  let json = JSON.parse(response.body);
+  if (!json.bugs || !json.bugs.length) {
+    return;
+  }
+
+  let bug = json.bugs[0];
+  let maybeUrl = includeUrl ? `${shortUrl} — ` : "";
+  let msg = `${maybeUrl}${bug.status} (${bug.assigned_to_detail.nick}) — ${bug.summary}`;
+  client.sendText(roomId, msg);
+
+  let timerId = setTimeout(() => {
+    roomCooldown.delete(bugNumber);
+  }, COOLDOWN_TIME);
+
+  roomCooldown.set(bugNumber, timerId);
+}
+
 async function expandBugNumber(client, roomId, msg) {
   var matches = null;
   while ((matches = BUG_NUMBER_REGEXP.exec(msg)) !== null) {
-    let bugNumber = matches[1];
-
-    let roomCooldown = cooldown.get(roomId);
-    if (typeof roomCooldown !== "undefined") {
-      if (typeof roomCooldown.get(bugNumber) !== "undefined") {
-        // Skip to avoid spamming the room.
-        continue;
-      }
-    } else {
-      roomCooldown = new Map();
-      cooldown.set(roomId, roomCooldown);
-    }
-
-    let url = `https://bugzilla.mozilla.org/rest/bug/${bugNumber}?include_fields=summary,assigned_to,status,resolution`;
-
-    let response = await request(url);
-    if (!response) {
-      continue;
-    }
-
-    let shortUrl = `https://bugzil.la/${bugNumber}`;
-    if (response.statusCode === 401) {
-      // Probably a private bug! Just send the basic information.
-      client.sendText(roomId, shortUrl);
-      continue;
-    }
-
-    if (response.statusCode !== 200) {
-      continue;
-    }
-
-    let json = JSON.parse(response.body);
-    if (!json.bugs || !json.bugs.length) {
-      continue;
-    }
-
-    let bug = json.bugs[0];
-    let msg = `${shortUrl} — ${bug.status} (${bug.assigned_to_detail.nick}) — ${bug.summary}`;
-    client.sendText(roomId, msg);
-
-    let timerId = setTimeout(() => {
-      roomCooldown.delete(bugNumber);
-    }, COOLDOWN_TIME);
-
-    roomCooldown.set(bugNumber, timerId);
+    await handleBug(client, roomId, matches[1], true);
+  }
+  matches = null;
+  while ((matches = BUG_URL_REGEXP.exec(msg)) !== null) {
+    await handleBug(client, roomId, matches[1], false);
   }
 }
 
